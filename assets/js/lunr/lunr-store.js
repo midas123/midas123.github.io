3d745451b6285761603cdbe42746ea96831d210c
midas123 <midasyk123@naver.com>
2019-12-30 01:11:17 +0000

    Site updated at 2019-12-30 01:11:17 UTC

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,131 @@
+var store = [{
+        "title": "윈도우10에서 jekyll 설치하기",
+        "excerpt":"윈도우에서 jekyll 설치 1.루비인스톨러 설치 (2.4 버전이상) ※gem은 프로그래밍 언어 루비에 패키지 매니져 2.윈도우 실행창에서 ‘cmd’ 입력해서 명령 프롬프트 실행 후 설치 여부 확인 ruby -v gem -v 3.jekyll 설치 및 확인 gem install jekyll bundler jekyll -v jekyll로 static 사이트 생성 및 실행 1.기본 static 사이트 생성(실행위치 C:\\Users\\username) jekyll...","categories": [],
+        "tags": ["jekyll"],
+        "url": "https://midas123.github.io/2019/01/21/install-jekyll-in-windows10.html",
+        "teaser":null},{
+        "title": "OFF-THE-BALL",
+        "excerpt":"프로젝트의 주제는 팀/개인 단위로 참여 가능한 축구경기 스케줄링 및 커뮤니티 사이트 입니다. 회원 관리와 로그인 기능을 구현하였고 javascript로 유효성검사 기능을 구현했습니다. OFF-THE-BALL - yk-dev-blog Home 회원가입 회원정보 수정 로그인 관리자 회원목록 관리자 회원상세 &#10094; &#10095; 제가 팀원으로써 담당한 파트는 아래와 같습니다. 담당 파트 - 회원가입 / 로그인 / 회원정보 수정...","categories": [],
+        "tags": ["struts","javascript","oracle","maven"],
+        "url": "https://midas123.github.io/2019/01/22/off-the-ball.html",
+        "teaser":null},{
+        "title": "ModuHome",
+        "excerpt":"두 번째 팀 프로젝트의 주제는 셀프 인테리어 쇼핑몰이면서 정보를 공유하는 커뮤니티 입니다. 제가 구현한 기능은 상품 게시판과 상품 상세페이지, 장바구니, 주문/결제 입니다. 상품 상세페이지에서 구매할 상품 옵션을 선택하거나 장바구니에서 상품 옵션 선택 및 수량 변경하는 등의 사용자 화면에서 동작하는 기능은 jquery로 구현하였습니다. 상품 게시판 정렬 순서, 회원 장바구니의 상품 수량...","categories": [],
+        "tags": ["spring-mvc","mybatis","ajax","jquery","oracle","maven"],
+        "url": "https://midas123.github.io/2019/01/26/ModuHome.html",
+        "teaser":null},{
+        "title": "ModuHome2",
+        "excerpt":"MODUHOME 프로젝트를 업그레이드 하였습니다. 그래서 ‘ModuHome2’라고 네이밍하였고 프로젝트의 부족한 부분을 보완하고 사이트를 실제로 배포하는 과정을 공부하였습니다. 프로젝트-github-링크 1.프로젝트 배포 환경 구성 처음에는 프로젝트를 수동으로 직접 배포하였습니다. Putty로 AWS 인스턴스와 SSH 통신하여 톰캣 서버를 설치하고 필요한 서버 설정을 한뒤, Filezilla로 프로젝트 .war파일을 업로드해서 수동 배포했습니다. 하지만 프로젝트에 변경 사항을 생길때마다 이...","categories": [],
+        "tags": ["junit","aws","docker","jenkins","maven"],
+        "url": "https://midas123.github.io/2019/02/07/ModuHome2.html",
+        "teaser":null},{
+        "title": "비교 연산자와 equals() 그리고 hashcode()",
+        "excerpt":"”==” 비교 연산자(relational operator) 두 객체를 비교할때 “==” 비교 연산자를 사용하면 두 객체의 참조(reference)가 같은지 비교하게 된다. 두 객체가 참조하는 객체의 메모리 주소가 같을 경우 true를 반환한다. 오로지 참조 객체만을 비교하기 때문에 두 객체의 값이 같은지 비교하는데 쓰일 수 없다. 반면에 int, char, boolean과 같은 primitive 타입의 값을 비교할때는 “==”...","categories": [],
+        "tags": ["java","equals","hashcode"],
+        "url": "https://midas123.github.io/2019/02/22/equality.html",
+        "teaser":null},{
+        "title": "자바 객체 정렬하기",
+        "excerpt":"java.util.Collections.sort() 자바 Collection 프레임워크는 객체 그룹을 저장하고 조작할 수 있는 다양한 구조체를 제공합니다. 이 구조체에 서는 대해서 탐색, 정렬, 삽입, 삭제 등에 조작이 가능합니다. interface - Set, List, Queue, Deque class - ArrayList, Vector, LinkedList, PriorityQueue, HashSet, LinkedHashSet, TreeSet 이런 컬렉션을 정렬하려면 Collection.sort()를 사용합니다. 그런데 이 메서드가 동작하기 위한 조건이...","categories": [],
+        "tags": ["java","comparable","comparator"],
+        "url": "https://midas123.github.io/2019/02/26/comparableVScomparator.html",
+        "teaser":null},{
+        "title": "JPA, Hibernate 기초",
+        "excerpt":"springboot으로 jpa와 hibernate를 사용하고 토이 프로젝트를 진행하다가 막히는 부분이 발생했는데 사용되는 생소한 용어와 모호한 개념을 먼저 잡고 넘어가기 위해서 이렇게 간단하게 정리하게 되었습니다. JPA JPA는 Java Persistence API의 약자, 오픈 소스이다. 자바 객체와 관계형 데이터베이스 사이에서 데이터를 맵핑(Object-relational mapping, ORM) 하고 저장, 접근, 관리하는 자바 클래스와 메서드가 포함되어 있다. -용어...","categories": [],
+        "tags": ["springboot","jpa","hibernate"],
+        "url": "https://midas123.github.io/2019/03/11/jpa.html",
+        "teaser":null},{
+        "title": "Springboot-SpringSecurity-JPA",
+        "excerpt":"이전에 진행한 팀 프로젝트에서는 spring과 mybatis를 다루었습니다. 그리고 mybatis에 대해 공부하면서 ORM과 hibernate에 대해서 알게 되었습니다. hibernate가 해외에서는 널리 사용되고 있고 국내에서도 사용이 늘고 있는 것으로 알고 있습니다. 그래서 작은 개인 프로젝트를 진행하며 공부해보았습니다. mybatis에서는 xml 설정 파일에 데이터베이스에 접근하는 쿼리를 작성하여 데이터베이스 위주로 기능하는 프로젝트를 만들었다면, 이번 개인 프로젝트에서는...","categories": [],
+        "tags": ["springboot","jpa","hibernate","spring-security","maven"],
+        "url": "https://midas123.github.io/2019/04/08/Springboot-Hibernate-project.html",
+        "teaser":null},{
+        "title": "React 기본 개념 정리",
+        "excerpt":"요즘은 리액트 같은 라이브러리 혹은 프레임워크를 이용해서 사용자의 브라우저에서 화면 렌더링을 담당하고 그때그때 데이터가 필요한 경우 서버에 요청하는 SPA(Single Page Application) 방식이 많이 사용되는 것으로 알고 있습니다. 이 방식은 서버 측에서는 자원의 사용을 최소화하고 사용자 측에서는 서버의 응답 시간이 줄어들기 때문에 사용 경험이 향상되는 장점이 있는 것으로 알고 있습니다. 이런...","categories": [],
+        "tags": ["javascript","react"],
+        "url": "https://midas123.github.io/2019/04/18/Reactjs-basic.html",
+        "teaser":null},{
+        "title": "React 프로젝트 세팅 방법",
+        "excerpt":"react 프로젝트 개발 환경 세팅을 정리했다. react는 유저 인터페이스를 만드는 라이브러리이므로 프론트 엔드만을 담당하기 때문에 백엔드에서 사용자 요청을 처리하기 위한 웹 서버가 추가로 필요하다. 그러므로 개발 단계에서 손 쉽게 사용할 수 있는 nodejs를 먼저 설치한다. 그리고 react 프로젝트 설치 하는데 쉽고 빠른 방법(create-react-app)과 복잡한 방법(webpack 설치 및 설정)이 있다.방법마다 장단점이...","categories": [],
+        "tags": ["javascript","react"],
+        "url": "https://midas123.github.io/2019/04/21/Reactjs-project.html",
+        "teaser":null},{
+        "title": "REST API 기본 개념",
+        "excerpt":"REST(Representational State Transfer)는 HTTP 프로토콜을 사용하는 웹 서비스 어플리케이션의 아키텍쳐 스타일이다. RESTful 웹 서비스는 REST를 베이스로 하는 웹 서비스를 말한다. 이 아키텍처의 특징은 리소스 중심으로 구성되며 서버가 클라이언트에게 오직 리소스에 대한 접근만을 제공하는 것이다. Resource 리소스는 REST에서 중요한 개념이다. 문서, 이미지, 일시적인 서비스, 어떤 리소스 그룹, 유저ID 혹은 사람 같이...","categories": [],
+        "tags": ["rest-api"],
+        "url": "https://midas123.github.io/2019/04/27/rest-basic.html",
+        "teaser":null},{
+        "title": "리눅스 mariaDB 세팅",
+        "excerpt":"리눅스(데비안) 환경에서 마리아DB 세팅 방법을 정리했습니다. mariaDB는 mysql 엔지니어가 만든 오픈 소스 데이터베이스 입니다. (GNU General Public License, version 2.) mysql과 호환될 뿐만 아니라 더 많은 기능과 빠른 성능을 가지고 있다고 합니다. 특징들 mariaDB 인스톨 서버/클라이언트 설치 sudo apt-get install mariadb-server mariadb-client ※dirmngr 미설치로 인한 에러시 아래코드로 설치 sudo apt-get...","categories": [],
+        "tags": ["linux","maria-db"],
+        "url": "https://midas123.github.io/2019/04/28/mariaDB-setting.html",
+        "teaser":null},{
+        "title": "오라클 JOIN",
+        "excerpt":"JOIN은 2개 이상의 테이블의 테이터를 조합한 결과를 만들때 사용한다. 오라클은 non-ANSI와 ANSI 조인(join) 문법을 제공한다. non-ANSI 조인은 오래전부터 사용되었고 지금도 여전히 인기가 많다. 이 문법은 FROM 절에 테이블을 열거하고 그 뒤에 WHERE 절에서 조인 조건을 정의한다. 이후 오라클 9i에서 도입된 ANSI 조인은 여러 이점이 있지만 오라클 개발자들은 습관적으로 또는 실행...","categories": [],
+        "tags": ["oracle","join"],
+        "url": "https://midas123.github.io/2019/05/03/oracle-join-syntax.html",
+        "teaser":null},{
+        "title": "스프링부트 RESTful 웹 서비스",
+        "excerpt":"앞서 스프링부트 + 스프링시큐리티를 사용해서 회원가입/로그인을 구현하였고 이어서 REST api 구조의 기본적인 게시판을 구현했습니다. 이번 프로젝트의 주제는 기본적인 게시판으로 REST API의 원칙을 준수하는 RESTful 웹 서비스의 백엔드 서버를 만드는데 집중했습니다. 게시판에서 사용자가 요청하게될 자원은 게시글(post)이 입니다. 그래서 post를 기준으로 일관적인 식별자 주소를 만들었습니다. post에 접근하는 주소는 기본적으로 ‘/post/{post_id}’ 형태이고 게시글...","categories": [],
+        "tags": ["rest-api","springboot","maven"],
+        "url": "https://midas123.github.io/2019/05/10/RESTful-webservice.html",
+        "teaser":null},{
+        "title": "ajax로 파일 업로드하기",
+        "excerpt":"기존 프로젝트에서 회원가입 시 이미지 파일을 업로드 할 수 있도록 수정하였습니다. 파일 정보를 저장하는 엔티티를 추가하고 파일 이름을 변경하여 경로에 저장하는 코드를 추가하였습니다. 그리고 ajax 코드에서 컨텐츠 타입을 multipart/form-data으로 변경하였습니다. 개발환경 springboot 2.1.3 jpa maven java8 변경 사항 UserImages 엔티티, DTO, DAO, 서비스 클래스 추가 자바 클래스에서 WebMvcConfigurer를 구현 후...","categories": [],
+        "tags": ["springboot","jpa","hibernate","ajax","maven"],
+        "url": "https://midas123.github.io/2019/05/15/ajax-file-upload.html",
+        "teaser":null},{
+        "title": "Web-Scraper",
+        "excerpt":"웹 페이지에서 게시글 제목과 url을 parsing하고 토큰화하여 DB에 저장하고 사용자가 그 내역을 키워드로 검색할 수 있도록 구현했습니다. 이 어플리케이션은 하나 이상의 웹 페이지에서 가져온 다수의 게시글을 한번에 검색 할 수 있는게 특징입니다. jsoup 라이브러리를 사용하였으며 사이트 마다 URL 형태가 다르기 때문에 웹 페이지를 parsing하는 메서드의 코드는 조금씩 달라야 했습니다. 그래서...","categories": [],
+        "tags": ["springboot","jpa","hibernate","gradle","maria-db","jsoup"],
+        "url": "https://midas123.github.io/2019/06/02/web-scraper.html",
+        "teaser":null},{
+        "title": "개발 단계에서 springboot-react 연동",
+        "excerpt":"이전 프로젝트의 프론트엔드를 reactjs로 만들 계획이며, 먼저 개발 단계에서 springboot REST api를 백엔드로 하고 reactjs로 프론트엔드로 세팅하는 방법을 알아보았습니다. 이 방법은 proxy를 사용하기 때문에 개발 단계에서만 유효합니다. 1.먼저 스프링부트 프로젝트(백엔드)와 리액트 프로젝트(프론트엔드) 생성합니다. 2.백엔드에서 http://localhost:8080/api/user로 요청시 user 정보를 리턴해주도록 합니다. @RestController @RequestMapping(\"/api\") public class UserRestController { @GetMapping(\"/user\") public Users getPost(Users...","categories": [],
+        "tags": ["springboot","react","fetch"],
+        "url": "https://midas123.github.io/2019/07/09/springboot-REST-api-react.html",
+        "teaser":null},{
+        "title": "웹 어플리케이션 내에 스케줄러 구현",
+        "excerpt":"스케줄러 API를 이용해서 WebScraper 프로젝트에 새로운 기능을 추가했습니다. 이전에는 사용자가 버튼을 눌러 수동으로 특정 사이트를 스크랩 하는 방식이었는데 스케줄러 기능을 추가하여 일정한 시간마다 사이트를 스크랩 하도록 구현했습니다. ‘스케줄러’로 검색해보니 java.util.Timer 클래스와 스프링에서 제공하는 TaskExecutor, TaskScheduler를 찾을 수 있었습니다. 그리고 프로젝트에 어느 부분에 이 코드를 구현하고 어떻게 동작하는지 알아보았습니다. 스케줄러는 구동...","categories": [],
+        "tags": ["springboot","timer"],
+        "url": "https://midas123.github.io/2019/07/19/web-scraper-scheduler.html",
+        "teaser":null},{
+        "title": "피보나치 수열과 재귀 함수 & call stack",
+        "excerpt":"피보나치수열을 재귀함수로 구현하는 방법을 공부하다가 재귀함수에 대해 더 깊은 이해를 하게 되어 이렇게 간단하게 정리하게 되었습니다. 이전까지 제가 이해한 재귀함수는 아래 코드처럼 함수가 함수 안에서 자기자신을 호출하는 것이었습니다. public int someMethod(){ //코드 return someMethod(); } 물론 기본 조건(base case) 없이 위 코드 처럼 함수를 호출하면 무한 재귀 호출에 빠지게 됩니다....","categories": [],
+        "tags": ["java","recursion","algorithm","call stack"],
+        "url": "https://midas123.github.io/2019/07/23/recursion-pibonacci.html",
+        "teaser":null},{
+        "title": "스프링시큐리티 - 필터 설정",
+        "excerpt":"스프링 시큐리티 내에서 요청에 대한 인증과 접근을 관리하는 시프링 시큐리티 필터에 대해 간단하게 알아보았습니다. The Security Filter Chain Spring Security’s web infrastructure는 서블릿 필터 기반입니다. 필터는 내부적으로 서블릿을 사용하거나 서블릿 기반에 프레임워크(ex. Spring MVC)를 사용하지 않습니다. 그러므로 특정 웹 기술과 깊게 연관되지 않습니다. 그리고 시큐리티 필터는 HttpServletRequest와 HttpServletResponse를 다룹니다. request가...","categories": [],
+        "tags": ["spring","spring-security","security-filter"],
+        "url": "https://midas123.github.io/2019/07/29/springsecurity-filter.html",
+        "teaser":null},{
+        "title": "스프링시큐리티 - authentication",
+        "excerpt":"프로젝트에서 간단하게 스프링-시큐리티를 적용해본 경험이 있습니다. 별도의 서비스 클래스를 만들고 UserDetailsService 인터페이스를 구현하여 loadUserByUsername()메서드에서 사용자가 입력한 정보와 DB에 있는 사용자 정보를 비교해서 유효성을 확인하도록 구현했었는데 인증 과정에 대해서 좀 더 구체적으로 공부하고자 스프링 문서를 보고 일부 내용을 정리했습니다. SecurityContextHolder SecurityContextHolder는 가장 기본적인 객체 입니다. 어플리케이션을 이용중인 principal을 포함하는 security context가...","categories": [],
+        "tags": ["spring","spring-security","authentication"],
+        "url": "https://midas123.github.io/2019/07/29/springsecurity.html",
+        "teaser":null},{
+        "title": "React 기본 개념 정리2",
+        "excerpt":"리액트 기본 개념 정리 첫 번째에 이어서 두 번째에서는 UI화면의 기능을 구현하는데 쓰이는 기본적인 리액트 기술에 대해서 정리했습니다. 이벤트 다루기 리액트에서 이벤트는 DOM 객체에서 이벤트와 매우 비슷합니다. 약간의 문법 차이가 있습니다. 1.리액트 이벤트는 대문자를 사용합니다. 2.JSX에서는 문자열 형태가 아니라 function 그대로 전달 합니다. //소문자, 문자열 &lt;button onclick=\"activateLasers()\"&gt; Activate Lasers &lt;/button...","categories": [],
+        "tags": ["javascript","react"],
+        "url": "https://midas123.github.io/2019/08/01/rest-basic2.html",
+        "teaser":null},{
+        "title": "Redux 기본 개념 정리",
+        "excerpt":"Redux는 자바스크립트 어플리케이션을 위한 state 관리 도구 입니다. 그리고 리액트와 사용하는 경우가 많습니다. 리액트는 기본적으로 컴포넌트에서 로컬 state를 관리하기 때문에 컴포넌트 숫자가 늘어날 수록 state 관리가 어렵습니다. Redux는 Store라는 공간에서 어플리케이션의 state를 저장하고 각 컴포넌트에서 저장된 state에 접근하고 업데이트 할 수 있게 해줍니다. why-use-redux-reasons-with-clear-examples Actions 액션은 store에 저장할 정보를 운반하는...","categories": [],
+        "tags": ["redux","react"],
+        "url": "https://midas123.github.io/2019/08/12/react-redux.html",
+        "teaser":null},{
+        "title": "Redux - 주요 API 함수",
+        "excerpt":"redux를 사용하면 react 앱에서 사용되는 모든 로컬 state를 한 곳에 저장하고 관리할 수 있다. 리액트 컴포넌트의 로컬 state를 저장하는 곳을 redux store라고 하며, 필요에 따라서 state를 업데이트하거나 그 값을 불러올 수도 있다. 이런 과정에서 사용되는 여러 api 함수가 있는데 각각의 역할과 사용 방법 등을 간단하게 정리해보려고 한다. 출처 링크에 가면...","categories": [],
+        "tags": ["redux","react"],
+        "url": "https://midas123.github.io/2019/08/17/react-redux-api-methods.html",
+        "teaser":null},{
+        "title": "자바 문자열- String, StringBuilder, StringBuffer",
+        "excerpt":"String 문자열을 다루는 알고리즘을 공부하다가 문자열을 변경하기 위해서 Stringbuilder를 사용하는 예제 코드를 보았습니다. String 문자열의 수정이 잦을 경우에 성능 이슈 때문에 Stringbuilder와 StringBuffer 같은 가변(mutable)를 사용해야 한다는 점을 알고 있었지만 자세한 특징과 차이점을 알고자 정리하게 되었습니다. String 자바의 String은 한번 만들어진 후 변경될 수 없는 불변(immutable)클래스 입니다. 메모리 사용량을 최적화하기...","categories": [],
+        "tags": ["java","string","stringbuilder","stringbuffer"],
+        "url": "https://midas123.github.io/2019/12/05/string-stringbuffer-stringbuilder-java.html",
+        "teaser":null},{
+        "title": "자바에서 null이란?",
+        "excerpt":"NullPointException은 프로그래밍을 하면서 자주 접하는 익셉션 중 하나이다. 레퍼런스 타입의 변수가 선언만 되고 초기화가 안된 상태에서 호출되면 발생하는 익셉션이다. Object obj; //obj 변수 선언 Object obj = new Object(\"abc\"); //선언 후 new 키워드로 초기화 위 코드처럼 레퍼런스 타입의 변수가 선언만 되었을때 객체는 null 값을 갖게 된다. 그런데 null은 무엇일까? 프로그래밍을...","categories": [],
+        "tags": ["java"],
+        "url": "https://midas123.github.io/2019/12/13/java-null.html",
+        "teaser":null}]
